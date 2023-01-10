41948f8bbb6ede4fac21bfee55474955e3f29b60
ivan <fsivan58@gmail.com>
2023-01-10 20:14:49 +0100

    Baudrate updated

--- code/src/test-CT-1.c
+++ code/src/test-CT-1.c
@@ -130,7 +130,7 @@ void init_uart(void)
     U1STAbits.URXISEL = 0;  // Interrupt on each character received
     
     // U1BRG = (Fcy/(16*Baud_rate) -1)
-    U1BRG = 3470;            // For 9600 bauds (119.808/2 MHz)/(16*9600) - 1
+    U1BRG = 389;            // For 9600 bauds (119.808/2 MHz)/(16*9600) - 1
 //    U1BRG = 32; //for 115200 bauds
     
     /* Configure interrupts */
