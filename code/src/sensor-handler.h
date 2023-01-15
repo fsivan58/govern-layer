767b4b255c0587b7dc2ebacf68713920f70a8599
ivan <fsivan58@gmail.com>
2023-01-15 22:26:11 +0100

    [21] implement sensor handler:     - Added sensor handler     - Mutual exclusion for sensor queue

--- code/src/sensor-handler.h
+++ code/src/sensor-handler.h
@@ -4,8 +4,12 @@
 #include "global.h"
 #include "definitions.h"
 
+extern Queue q;
 extern Queue sensor_q;
 
-void handle_sensor();
+extern Tsemaphore sensor_s;
+
+void init_sensor_q(int size);
+void handle_sensor(int msg);
 
 #endif /* SENSOR_HANDLER */
